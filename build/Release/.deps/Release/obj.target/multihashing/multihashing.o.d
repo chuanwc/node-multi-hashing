d95a1eabecfd2ee6cb5c867b55c36962a5ca7650
surinserver <skzipa@windowslive.com>
2017-11-03 22:04:47 +0700

    updata

---
+++ build/Release/.deps/Release/obj.target/multihashing/multihashing.o.d
@@ -0,0 +1,58 @@
+cmd_Release/obj.target/multihashing/multihashing.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.node-gyp/0.10.25/src -I/root/.node-gyp/0.10.25/deps/uv/include -I/root/.node-gyp/0.10.25/deps/v8/include -I../crypto -I../sha3 -I../../../../nan  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-tree-sink -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=c++0x -MMD -MF ./Release/.deps/Release/obj.target/multihashing/multihashing.o.d.raw   -c -o Release/obj.target/multihashing/multihashing.o ../multihashing.cc
+Release/obj.target/multihashing/multihashing.o: ../multihashing.cc \
+ /root/.node-gyp/0.10.25/src/node.h \
+ /root/.node-gyp/0.10.25/deps/uv/include/uv.h \
+ /root/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-unix.h \
+ /root/.node-gyp/0.10.25/deps/uv/include/uv-private/ngx-queue.h \
+ /root/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-linux.h \
+ /root/.node-gyp/0.10.25/deps/v8/include/v8.h \
+ /root/.node-gyp/0.10.25/deps/v8/include/v8stdint.h \
+ /root/.node-gyp/0.10.25/src/node_object_wrap.h \
+ /root/.node-gyp/0.10.25/src/node.h \
+ /root/.node-gyp/0.10.25/src/node_buffer.h ../../../../nan/nan.h \
+ /root/.node-gyp/0.10.25/src/node_version.h \
+ /root/.node-gyp/0.10.25/src/node_object_wrap.h ../../../../nan/nan_new.h \
+ ../../../../nan/nan_implementation_pre_12_inl.h ../bcrypt.h ../keccak.h \
+ ../quark.h ../blake2s.h ../scryptjane.h ../scryptn.h ../skein.h ../x11.h \
+ ../groestl.h ../blake.h ../fugue.h ../qubit.h ../hefty1.h ../shavite3.h \
+ ../x13.h ../nist5.h ../sha1.h ../x15.h ../fresh.h ../Lyra2RE.h \
+ ../Lyra2.h ../Lyra2REV2.h ../Lyra2Z.h
+../multihashing.cc:
+/root/.node-gyp/0.10.25/src/node.h:
+/root/.node-gyp/0.10.25/deps/uv/include/uv.h:
+/root/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-unix.h:
+/root/.node-gyp/0.10.25/deps/uv/include/uv-private/ngx-queue.h:
+/root/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-linux.h:
+/root/.node-gyp/0.10.25/deps/v8/include/v8.h:
+/root/.node-gyp/0.10.25/deps/v8/include/v8stdint.h:
+/root/.node-gyp/0.10.25/src/node_object_wrap.h:
+/root/.node-gyp/0.10.25/src/node.h:
+/root/.node-gyp/0.10.25/src/node_buffer.h:
+../../../../nan/nan.h:
+/root/.node-gyp/0.10.25/src/node_version.h:
+/root/.node-gyp/0.10.25/src/node_object_wrap.h:
+../../../../nan/nan_new.h:
+../../../../nan/nan_implementation_pre_12_inl.h:
+../bcrypt.h:
+../keccak.h:
+../quark.h:
+../blake2s.h:
+../scryptjane.h:
+../scryptn.h:
+../skein.h:
+../x11.h:
+../groestl.h:
+../blake.h:
+../fugue.h:
+../qubit.h:
+../hefty1.h:
+../shavite3.h:
+../x13.h:
+../nist5.h:
+../sha1.h:
+../x15.h:
+../fresh.h:
+../Lyra2RE.h:
+../Lyra2.h:
+../Lyra2REV2.h:
+../Lyra2Z.h:
